5549bb02f5ab51e4b3faf2b594d247a16ab5430b
Muhammad Kumail <mkumailofficial@gmail.com>
2020-06-24 06:16:14 +0500

    setting up express server and cleaned files

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { data: "" };
+  }
+
+  callAPI() {
+    fetch('http://localhost:4000')
+      .then(res => res.text())
+      .then(res => this.setState({ data: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.data}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
