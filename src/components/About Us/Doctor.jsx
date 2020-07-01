0e916c4d94deafcb15eb995dfdb7627c3bf5d4b5
Anagha <dpatil.anagha@gmail.com>
2020-07-01 20:21:16 +0530

    .

--- src/components/About Us/Doctor.jsx
+++ src/components/About Us/Doctor.jsx
@@ -3,11 +3,9 @@ import Navbar from "../../UI/Navbar/Navbar";
 
 function Doctor(){
   return (
-    <div>
+  
     <h1 ABOUT US/>
-    <br>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pharetra lacus non nulla congue luctus.</p>
-    </div>
+
   );
 };
 
