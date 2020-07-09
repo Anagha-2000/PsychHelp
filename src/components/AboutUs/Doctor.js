0c30d19359c6cf7d5bf85145f6ffffe5e5d123c1
Anagha <dpatil.anagha@gmail.com>
2020-07-09 19:06:02 +0530

    .

--- src/components/AboutUs/Doctor.js
+++ src/components/AboutUs/Doctor.js
@@ -5,6 +5,17 @@ const Doctor = () =>{
   <div>
   <Navbar />
     <h1> ABOUT US</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque,
+    sapien in pretium molestie, mauris odio luctus urna, quis feugiat dolor enim vitae dolor.
+    Quisque in elit urna. Nulla placerat dignissim quam quis commodo. Etiam pulvinar,
+    eros id euismod vulputate, libero ipsum tristique velit, quis ullamcorper massa
+    quam vel turpis. Morbi luctus dictum erat, finibus ultricies leo.
+    Aliquam at dolor in lacus finibus iaculis id id leo. In mollis at mi et auctor.
+    Curabitur tincidunt non diam a imperdiet. Nullam libero lectus, efficitur
+    quis suscipit sed, suscipit at diam. Donec cursus eu mauris vitae rutrum.
+    Aenean a elit ac metus sollicitudin interdum. Quisque pulvinar ante eleifend,
+    tincidunt nisl sit amet, tristique lectus. Aliquam eu luctus mauris.
+     Fusce vitae eleifend nibh. </p>
 </div>
   );
 }
