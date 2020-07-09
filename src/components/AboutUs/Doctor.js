827f0932e32284249b8349d015c411aa72d1eef0
Anagha <dpatil.anagha@gmail.com>
2020-07-09 12:09:58 +0530

    .

--- src/components/AboutUs/Doctor.js
+++ src/components/AboutUs/Doctor.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Navbar from "../../UI/Navbar/Navbar";
 
 const Doctor = () =>{
   return (
   <div>
-    <Navbar />
+
     <h1> ABOUT US</h1>
 </div>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Navbar from "../../UI/Navbar/Navbar";
 const Login = () => {
     return (
         <div>
