760f61af8c0d7168644eb18b7c6ddc664c7343cb
Anagha <dpatil.anagha@gmail.com>
2020-07-15 18:26:53 +0530

    .

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -4,9 +4,9 @@ function FAQs(){
   return (
     <div>
 <div id="FAQs">
-    <h1 id="heading"><b> FAQs </b> </h1>
+    <h1 id="heading"><b> Frequently Asked Questions </b> </h1>
 
-    <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>          Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>          Sed iaculis justo ut nibh vehicula, at mattis orci interdum.</p>
     <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>          Nulla interdum nulla a felis iaculis malesuada.</p>
     <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>          Sed quis justo sit amet dolor cursus varius ut non metus.</p>
