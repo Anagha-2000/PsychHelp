57982f21acee7ffd8aac884e313fd78096368da4
Anagha <dpatil.anagha@gmail.com>
2020-07-15 19:54:50 +0530

    .

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,35 +1,47 @@
 import React from 'react';
 import Navbar from "../../UI/Navbar/Navbar";
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Footer from '../Footer';
 const Login = () => {
 
     return (
 
         <div>
             <Navbar />
+      <div class = "login-select">
+            <CardDeck>
+    <Card>
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>Login for...</Card.Title>
+        <Card.Text>
+          <h3>Doctor</h3>
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">Are you here to help?</small>
+      </Card.Footer>
+    </Card>
+    <Card>
 
-            <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>Login for...</Card.Title>
+        <Card.Text>
+          <h3>Patient</h3>
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">Are you here to seek help?</small>
+      </Card.Footer>
+    </Card>
+</CardDeck>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+</div>
 
+<Footer />
         </div>
     );
 }
