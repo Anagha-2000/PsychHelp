8e9d676c6445856b2651c91f3901d9879d7530c0
Anagha <dpatil.anagha@gmail.com>
2020-07-15 13:48:38 +0530

    .

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -1,17 +1,19 @@
 import React from "react";
-
+import Footer from './Footer';
 function FAQs(){
   return (
+    <div>
 <div id="FAQs">
     <h1 id="heading"><b> FAQs </b> </h1>
-    <ul>
-    <li class = "lists"><img src="https://img.icons8.com/flat_round/100/000000/arrow-right.png"/>    Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-    <li class = "lists"><img src="https://img.icons8.com/flat_round/100/000000/arrow-right.png"/>    Sed iaculis justo ut nibh vehicula, at mattis orci interdum.</li>
-    <li class = "lists"><img src="https://img.icons8.com/flat_round/100/000000/arrow-right.png"/>    Nulla interdum nulla a felis iaculis malesuada.</li>
-    <li class = "lists"><img src="https://img.icons8.com/flat_round/100/000000/arrow-right.png"/>    Sed quis justo sit amet dolor cursus varius ut non metus.</li>
-    </ul>
+
+    <li class = "lists"><img src="https://img.icons8.com/flat_round/64/000000/arrow-right.png"/>    Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+    <li class = "lists"><img src="https://img.icons8.com/flat_round/64/000000/arrow-right.png"/>    Sed iaculis justo ut nibh vehicula, at mattis orci interdum.</li>
+    <li class = "lists"><img src="https://img.icons8.com/flat_round/64/000000/arrow-right.png"/>    Nulla interdum nulla a felis iaculis malesuada.</li>
+    <li class = "lists"><img src="https://img.icons8.com/flat_round/64/000000/arrow-right.png"/>    Sed quis justo sit amet dolor cursus varius ut non metus.</li>
 
 
+</div>
+  <Footer />
   </div>
 );
 }
