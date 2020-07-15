a76cc61b30b55c069489228c52ee013d6ca28bba
Anagha <dpatil.anagha@gmail.com>
2020-07-15 19:37:43 +0530

    .

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function FAQs(){
   return (
@@ -15,7 +17,13 @@ function FAQs(){
     <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>          Nulla interdum nulla a felis iaculis malesuada.</p>
     <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>          Sed quis justo sit amet dolor cursus varius ut non metus.</p>
 
-
+    <Card style={{ width: '18rem' }}>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </Card>
 
 </div>
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from "../../UI/Navbar/Navbar";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 const Login = () => {
 
     return (
@@ -7,7 +9,26 @@ const Login = () => {
         <div>
             <Navbar />
 
-        
+            <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
 
         </div>
     );
