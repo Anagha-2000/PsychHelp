7eb2fe79ef6681e02f5e444655469097dee5ff64
Anagha <dpatil.anagha@gmail.com>
2020-07-15 10:44:24 +0530

    .

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -3,7 +3,13 @@ import React from "react";
 function FAQs(){
   return (
 <div id="FAQs">
-    <h1> FAQs </h1>;
+    <h1 id="heading"><b> FAQs </b> </h1>
+    <ul>
+    <li class = "lists">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+    <li class = "lists">Sed iaculis justo ut nibh vehicula, at mattis orci interdum.</li>
+    <li class = "lists">Nulla interdum nulla a felis iaculis malesuada.</li>
+    <li class = "lists">Sed quis justo sit amet dolor cursus varius ut non metus.</li>
+    </ul>
 
 
   </div>
