b27b23d4d7d0ab523331bcee97dd1c36f267afae
Anagha <dpatil.anagha@gmail.com>
2020-07-15 20:52:25 +0530

    .

--- src/components/Login/Login-Navbar.js
+++ src/components/Login/Login-Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from './Navbar.module.css';
+import classes from '../Navbar.module.css';
 import { Link } from "react-router-dom";
 
 const LoginNavbar = () => {
