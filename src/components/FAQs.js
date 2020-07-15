ee8987afbc10259a8bfc1faf96d63bcda91b02d9
Anagha <dpatil.anagha@gmail.com>
2020-07-15 14:03:03 +0530

    .

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -14,11 +14,11 @@ function FAQs(){
     <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>    Sed iaculis justo ut nibh vehicula, at mattis orci interdum.</p>
     <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>    Nulla interdum nulla a felis iaculis malesuada.</p>
     <p class = "lists"><img src="https://img.icons8.com/flat_round/30/000000/arrow-right.png"/>    Sed quis justo sit amet dolor cursus varius ut non metus.</p>
-    
 
+<Footer />
 
 </div>
-  <Footer />
+
   </div>
 );
 }
