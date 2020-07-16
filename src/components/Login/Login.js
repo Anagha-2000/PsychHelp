a983572c8ef94dbeb7cab44880d9fa605ac68ec3
Anagha <dpatil.anagha@gmail.com>
2020-07-16 14:53:34 +0530

    .

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
 
     return (
 
-        <div class = "transparenttextures">
+        <div>
             <LoginNavbar />
       <div class = "login-select">
             <CardDeck>
