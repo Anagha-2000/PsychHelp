7f714c82e7f2ac94123ce2b51e790c84b3d3a7f2
Anagha <dpatil.anagha@gmail.com>
2020-07-16 09:07:12 +0530

    .

--- src/components/Login/Login-Doctor.js
+++ src/components/Login/Login-Doctor.js
@@ -21,23 +21,32 @@ const LoginDoctor = () => {
             </ul>
         </nav>
 
-        <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+        <Card>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+          <Card.Body>
+          <Form>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
+          </Card.Body>
+          
+        </Card>
+
+
+
 </div>
     );
 };
