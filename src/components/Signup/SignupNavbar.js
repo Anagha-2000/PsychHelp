4dcdcf5866387641a820499233e545f9e39fe8dd
Anagha <dpatil.anagha@gmail.com>
2020-07-20 10:58:08 +0530

    .

--- src/components/Signup/SignupNavbar.js
+++ src/components/Signup/SignupNavbar.js
@@ -12,11 +12,6 @@ const SignupNavbar = () => {
                 <h3 className={classes.title}>PsychHelp</h3>
             </Link>
 
-            <ul className={classes.navLinks}>
-                <Link style={style} to="/Blog">
-                    <li className={classes.item}></li>
-                </Link>
-              </ul>
         </nav>
     );
 };
